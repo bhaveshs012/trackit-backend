0464c4379b9cecf2ffe5fd79b5635edbf23d7492
Bhavesh Sengunthar <bhaveshss012@gmail.com>
2024-11-21 23:05:51 +0530

    Update cookiesOptions.js

--- src/utils/cookiesOptions.js
+++ src/utils/cookiesOptions.js
@@ -1,13 +1,13 @@
 const accessTokenCookieOptions = {
   httpOnly: false, // Accessible from JavaScript
-  secure: true, // Use true in production (HTTPS)
+  secure: false, // Use true in production (HTTPS)
   sameSite: "Lax",
   maxAge: 1 * 60 * 60 * 1000, // 1 hour
 };
 
 const refreshTokenCookieOptions = {
   httpOnly: true,
-  secure: true, // Use true in production (HTTPS)
+  secure: false, // Use true in production (HTTPS)
   sameSite: "Lax",
   maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
 };
